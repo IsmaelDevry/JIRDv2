d11652106a696606061ccd04cde5dafbe9d0b750
IsmaelDevry <124474296+IsmaelDevry@users.noreply.github.com>
2023-02-05 21:51:30 -0700

    First Commit

---
+++ src/main/java/jird/jird/JIRD.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package jird.jird;
+
+/**
+ *
+ * @author Ismael
+ */
+public class JIRD {
+    public class Employee {
+
+
+            } 
+        public class Warehouse {
+
+
+            } 
+        public class Tool {
+
+
+            } 
+        public class Job { 
+
+
+}
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+    
+    public static void login() {
+    //used to log into system by Employee
+    
+    }
+    
+    public static void searchTool() {
+    //contains all tools that are in the system
+    
+    }
+    
+    public static void editTool() {
+    //used to make changes toward quantity,availability, etc...
+    
+    }
+    
+    public static void requestDelivery() {
+    //used to order supplies or tools
+    
+    }
+    
+    
+}
+
+
+
+
